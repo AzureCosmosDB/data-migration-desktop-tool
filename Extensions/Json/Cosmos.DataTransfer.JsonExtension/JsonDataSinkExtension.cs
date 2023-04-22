a6e9dcad0e1e60025dfd4481a782dffa81c7a1bb
Hasan Savran <hsavran@kent.edu>
2023-04-21 21:47:30 -0400

    S3 support for JSONDataSinkExtension

--- Extensions/Json/Cosmos.DataTransfer.JsonExtension/JsonDataSinkExtension.cs
+++ Extensions/Json/Cosmos.DataTransfer.JsonExtension/JsonDataSinkExtension.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.Composition;
 using System.Text.Json;
+using System.Threading;
 using Cosmos.DataTransfer.Interfaces;
 using Cosmos.DataTransfer.JsonExtension.Settings;
 using Microsoft.Extensions.Configuration;
@@ -20,21 +21,44 @@ namespace Cosmos.DataTransfer.JsonExtension
             if (settings.FilePath != null)
             {
                 logger.LogInformation("Writing to file '{FilePath}'", settings.FilePath);
-                await using var stream = File.Create(settings.FilePath);
-                await using var writer = new Utf8JsonWriter(stream, new JsonWriterOptions
-                {
-                    Indented = settings.Indented
-                });
-                writer.WriteStartArray();
-
-                await foreach (var item in dataItems.WithCancellation(cancellationToken))
-                {
-                    DataItemJsonConverter.WriteDataItem(writer, item, settings.IncludeNullFields);
-                }
-
-                writer.WriteEndArray();
+                await SaveFile(dataItems, settings, cancellationToken);
+                
                 logger.LogInformation("Completed writing data to file '{FilePath}'", settings.FilePath);
+                if (settings.UploadToS3 == true)
+                {
+                    if (settings.S3Region != null && settings.S3BucketName != null && settings.S3AccessKey != null && settings.S3SecretKey != null)
+                    {
+                        logger.LogInformation("Saving file to AWS S3 Bucket '{BucketName}'", settings.S3BucketName);
+                        await SaveToS3(settings, cancellationToken);
+                    }
+                    else
+                    {
+                        logger.LogError("S3 Requires S3Region, S3BucketName, S3AccessKey, and S3SecretKey to be set.");
+                    }
+                }
             }
+        }
+
+        private async Task SaveFile(IAsyncEnumerable<IDataItem> dataItems, JsonSinkSettings settings, CancellationToken cancellationToken = default)
+        {
+            await using var stream = File.Create(settings.FilePath);
+            await using var writer = new Utf8JsonWriter(stream, new JsonWriterOptions
+            {
+                Indented = settings.Indented
+            });
+            writer.WriteStartArray();
+
+            await foreach (var item in dataItems.WithCancellation(cancellationToken))
+            {
+                DataItemJsonConverter.WriteDataItem(writer, item, settings.IncludeNullFields);
+            }
+
+            writer.WriteEndArray();
+        }
+        private async Task SaveToS3(JsonSinkSettings settings, CancellationToken cancellationToken)
+        {
+            S3Writer.InitializeS3Client(settings.S3AccessKey, settings.S3SecretKey, settings.S3Region);
+            await S3Writer.WriteToS3(settings.S3BucketName, settings.FilePath, cancellationToken);
         }
     }
 }
--- Extensions/Json/Cosmos.DataTransfer.JsonExtension/Settings/JsonSinkSettings.cs
+++ Extensions/Json/Cosmos.DataTransfer.JsonExtension/Settings/JsonSinkSettings.cs
@@ -10,5 +10,10 @@ namespace Cosmos.DataTransfer.JsonExtension.Settings
 
         public bool IncludeNullFields { get; set; }
         public bool Indented { get; set; }
+        public bool UploadToS3 { get; set; }
+        public string S3Region { get; set; }
+        public string S3BucketName { get; set; }
+        public string S3AccessKey { get; set; }
+        public string S3SecretKey { get; set; }
     }
 }
