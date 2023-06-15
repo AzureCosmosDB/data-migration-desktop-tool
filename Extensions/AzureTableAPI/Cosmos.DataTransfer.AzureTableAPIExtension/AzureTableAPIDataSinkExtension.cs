54dc2818ae1d8434efe462f53b3293c63ed9dbc6
Roman <roman.alshevscky@gmail.com>
2023-06-15 16:08:21 +0300

    Update Azure table sink logic

--- Extensions/AzureTableAPI/Cosmos.DataTransfer.AzureTableAPIExtension/AzureTableAPIDataSinkExtension.cs
+++ Extensions/AzureTableAPI/Cosmos.DataTransfer.AzureTableAPIExtension/AzureTableAPIDataSinkExtension.cs
@@ -3,6 +3,7 @@ using Azure.Data.Tables;
 using Cosmos.DataTransfer.AzureTableAPIExtension.Data;
 using Cosmos.DataTransfer.AzureTableAPIExtension.Settings;
 using Cosmos.DataTransfer.Interfaces;
+using Microsoft.Azure.Cosmos;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
@@ -23,14 +24,52 @@ namespace Cosmos.DataTransfer.AzureTableAPIExtension
 
             await tableClient.CreateIfNotExistsAsync();
 
-            var createTasks = new List<Task>();
-            await foreach(var item in dataItems.WithCancellation(cancellationToken))
+            var upsertTasks = new List<Task>();
+            string currentDir = Directory.GetCurrentDirectory();
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+
+            int totalCount = 0;
+            int failedCount = 0;
+            int succeedCount = 0;
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(currentDir, "error.log")))
             {
-               var entity = item.ToTableEntity(settings.PartitionKeyFieldName, settings.RowKeyFieldName);
-               createTasks.Add(tableClient.AddEntityAsync(entity));
+                await foreach(var item in dataItems.WithCancellation(cancellationToken))
+                {
+                   totalCount++;
+                   var entity = item.ToTableEntity(settings.PartitionKeyFieldName, settings.RowKeyFieldName);
+                   var task = tableClient.UpsertEntityAsync(entity).ContinueWith(itemResponse =>
+                    {
+                        if (!itemResponse.IsCompletedSuccessfully)
+                        {
+                            AggregateException innerExceptions = itemResponse.Exception.Flatten();
+                            if (innerExceptions.InnerExceptions.FirstOrDefault(innerEx => innerEx is CosmosException) is CosmosException cosmosException)
+                            {
+                                throw cosmosException;
+                            }
+                            else
+                            {
+                                string entityId = $"Partition key: {entity.PartitionKey}. Row key: {entity.RowKey}.";
+                                string message = $"{entityId} Exception {innerExceptions.InnerExceptions.FirstOrDefault()}.";
+                                outputFile.WriteLine(message);
+                                Console.WriteLine(message);
+                                failedCount++;
+                            }
+                        }
+                        else
+                        {
+                            succeedCount++;
+                        }
+                    });
+                    upsertTasks.Add(task);
+                }
             }
 
-            await Task.WhenAll(createTasks);
+            await Task.WhenAll(upsertTasks);
+
+            watch.Stop();
+            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+            Console.WriteLine($"Total entities: {totalCount}. Succeed: {succeedCount}. Failed: {failedCount}.");
         }
 
         public IEnumerable<IDataExtensionSettings> GetSettings()
