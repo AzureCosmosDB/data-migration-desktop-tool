bf935b9941810453a1c680383fc36a6b5d9f550e
John Bowen <john@bowencode.net>
2023-08-07 19:15:51 -0700

    Saving window position

--- App/Cosmos.DataTransfer.App.Windows/MainWindow.xaml.cs
+++ App/Cosmos.DataTransfer.App.Windows/MainWindow.xaml.cs
@@ -25,6 +25,34 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        if (Properties.Settings.Default.WindowSize.Width > 0)
+        {
+            Width = Properties.Settings.Default.WindowSize.Width;
+            Height = Properties.Settings.Default.WindowSize.Height;
+            Left = Properties.Settings.Default.WindowPosition.X;
+            Top = Properties.Settings.Default.WindowPosition.Y;
+        }
+
+        if (Properties.Settings.Default.WindowMaximized)
+            WindowState = WindowState.Maximized;
+
+        Closing += (s, e) =>
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                Properties.Settings.Default.WindowPosition = new System.Drawing.Point((int)RestoreBounds.Location.X, (int)RestoreBounds.Location.Y);
+                Properties.Settings.Default.WindowSize = new System.Drawing.Size((int)RestoreBounds.Size.Width, (int)RestoreBounds.Size.Height);
+                Properties.Settings.Default.WindowMaximized = true;
+            }
+            else
+            {
+                Properties.Settings.Default.WindowPosition = new System.Drawing.Point((int)Left, (int)Top);
+                Properties.Settings.Default.WindowSize = new System.Drawing.Size((int)Width, (int)Height);
+                Properties.Settings.Default.WindowMaximized = false;
+            }
+            Properties.Settings.Default.Save();
+        };
     }
 
     private void AllowOnlyNumbers(object sender, TextCompositionEventArgs e)
