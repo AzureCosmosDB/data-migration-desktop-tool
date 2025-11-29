51693819588f7aba4a3ae1bac82c4d6f1f64a781
Phil Nachreiner <philnach@microsoft.com>
2025-11-29 06:48:15 -0800

    Add a test for the SSL disable

--- Extensions/Cosmos/Cosmos.DataTransfer.CosmosExtension.UnitTests/CertificateConfigurationTests.cs
+++ Extensions/Cosmos/Cosmos.DataTransfer.CosmosExtension.UnitTests/CertificateConfigurationTests.cs
@@ -1,5 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 
+using Moq;
+
 namespace Cosmos.DataTransfer.CosmosExtension.UnitTests
 {
     [TestClass]
@@ -37,6 +39,36 @@ namespace Cosmos.DataTransfer.CosmosExtension.UnitTests
             Assert.AreEqual(0, validationResults.Count, "Settings should validate with DisableSslValidation=false");
         }
 
+            [TestMethod]
+            public void CreateClient_WithDisableSslValidation_LogsWarningAndSetsCallback()
+            {
+                var loggerMock = new Moq.Mock<Microsoft.Extensions.Logging.ILogger>();
+                var settings = new TestableCosmosSettings
+                {
+                    ConnectionString = "AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDj...",
+                    Database = "testDb",
+                    Container = "testContainer",
+                    DisableSslValidation = true
+                };
+
+                var client = Cosmos.DataTransfer.CosmosExtension.CosmosExtensionServices.CreateClient(settings, "TestDisplay", loggerMock.Object);
+
+                // Verify warning was logged
+                loggerMock.Verify(
+                    l => l.Log(
+                        Microsoft.Extensions.Logging.LogLevel.Warning,
+                        Moq.It.IsAny<Microsoft.Extensions.Logging.EventId>(),
+                        Moq.It.Is<Moq.It.IsAnyType>((v, t) => v.ToString().Contains("SSL certificate validation is DISABLED")),
+                        Moq.It.IsAny<System.Exception>(),
+                        Moq.It.IsAny<System.Func<Moq.It.IsAnyType, System.Exception, string>>()),
+                    Moq.Times.AtLeastOnce);
+
+                // Use reflection to get the ServerCertificateCustomValidationCallback
+                var optionsField = typeof(Microsoft.Azure.Cosmos.CosmosClient).GetField("clientOptions", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+                var clientOptions = optionsField?.GetValue(client) as Microsoft.Azure.Cosmos.CosmosClientOptions;
+                Assert.IsNotNull(clientOptions, "CosmosClientOptions should not be null");
+                Assert.IsNotNull(clientOptions.ServerCertificateCustomValidationCallback, "ServerCertificateCustomValidationCallback should be set");
+            }
 
         private class TestableCosmosSettings : CosmosSettingsBase
         {
